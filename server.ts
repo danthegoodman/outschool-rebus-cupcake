8f6793283d28114be3d7a7e70af7c09ade5e651f
Larry Sharette <larry@outschool.com>
2021-10-06 12:39:55 -0700

    updated name of route

--- server.ts
+++ server.ts
@@ -1,15 +1,19 @@
 import {handlePublicFile} from "./server/public-files.ts";
 import {textResponse} from "./server/util.ts";
-import {handleRebusList} from "./server/rebus-db.ts";
-import {initEmojiMapping} from "./server/emoji-mapping.ts";
+import { handleRebusRequest } from "./server/rebus-db.ts";
+import { initEmojiMapping } from "./server/emoji-mapping.ts";
 
-async function handleRequest(request: Request){
-  const {pathname} = new URL(request.url);
+async function handleRequest(request: Request) {
+  const { pathname } = new URL(request.url);
   switch (pathname) {
-    case '/': return handlePublicFile('/index.html');
-    case '/api/rebus-list': return handleRebusList();
-    case '/favicon.ico': return new Response(null, {status:404});
-    default: return handlePublicFile(pathname);
+    case "/":
+      return handlePublicFile("/index.html");
+    case "/api/rebus":
+      return handleRebusRequest(request);
+    case "/favicon.ico":
+      return new Response(null, { status: 404 });
+    default:
+      return handlePublicFile(pathname);
   }
 }
 
