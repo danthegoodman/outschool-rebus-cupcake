8107a45259eaa0e6d585839f2c7b62d8a58f94bc
Larry Sharette <larry@outschool.com>
2021-10-06 11:49:41 -0700

    updated .gitignore

--- server/rebus-db.ts
+++ server/rebus-db.ts
@@ -1,16 +1,51 @@
 import {jsonResponse} from "./util.ts";
 import {getEmojiUrl} from "./emoji-mapping.ts";
+import {
+  DynamoDBClient,
+  ScanCommand,
+  PutItemCommand,
+} from "https://cdn.skypack.dev/@aws-sdk/client-dynamodb@3.20.0?dts";
 
-export async function handleRebusList(){
-  return jsonResponse([
-    {puzzle: parseRebus(':teapot::four_leaf_clover:'), solution: 'Potluck'},
-    {puzzle: parseRebus('(:ear::heavy_minus_sign::a:):trophy:'), solution: 'Erwin'}
-  ])
+const client = new DynamoDBClient({
+  region: "us-east-1",
+  credentials: {
+    accessKeyId: Deno.env.get("AWS_ACCESS_KEY_ID"),
+    secretAccessKey: Deno.env.get("AWS_SECRET_ACCESS_KEY"),
+    sessionToken: Deno.env.get("AWS_SESSION_TOKEN"),
+  },
+});
+
+interface RebusPuzzle {
+  puzzle: string;
+  solution: string;
 }
 
-type RebusDatum =
-  | {text:string}
-  | {image:string, shortName: string};
+export async function handleRebusList() {
+  const items = await testScan([]);
+  return jsonResponse(items);
+}
+
+type RebusDatum = { text: string } | { image: string; shortName: string };
+
+async function testScan(items: any[], startKey?: any): Promise<RebusPuzzle[]> {
+  if (items.length > 0 && startKey === undefined) {
+    return items;
+  }
+  const { Items, LastEvaluatedKey } = await client.send(
+    new ScanCommand({
+      TableName: "cupcake-2021q3-rebus-puzzles",
+      ExclusiveStartKey: startKey,
+    })
+  );
+
+  const newItems =
+    Items?.map((item) => ({
+      puzzle: parseRebus(item.p.S ?? ""),
+      solution: item.s.S,
+    })) ?? [];
+
+  return testScan(newItems, LastEvaluatedKey);
+}
 
 export function parseRebus(puzzle: string): RebusDatum[] {
   let items: RebusDatum[] = [];
