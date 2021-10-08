4938da940d79a0989d2d7ed130ccdf6a256c6e71
Ana Simmons <ana.simmons@outschool.com>
2021-10-08 11:55:31 -0400

    styles: slightly better mobile experience for /start

--- frontend/src/pages/Start.tsx
+++ frontend/src/pages/Start.tsx
@@ -7,22 +7,22 @@ import { SingleEmoji } from "../component/RebusPuzzle";
 export default function StartMenu() {
   return (
     <>
-      <Row className="py-4 mx-2" xs={3}>
-        <Col xs={1} md={4} />
-        <Col className="px-4 mx-2" xs={10} md={4}>
+      <Row className="py-4">
+        <Col xs={0} md={4} />
+        <Col className="px-4" xs={12} md={4}>
           <Boombox />
         </Col>
-        <Col xs={1} md={4} />
+        <Col xs={0} md={4} />
       </Row>
-      <Row className="py-4 mx-2">
-        <Col>
-          <Jumbotron className="m-4 p-4 bg-mango rounded">
+      <Row className="py-4 d-flex justify-content-center">
+        <Col xs={12} md={10}>
+          <Jumbotron className="p-4 bg-mango rounded">
             <Row>
               <Col>
-                <h1 className="text-white display-2 bg-blurple rounded d-flex justify-content-center py-2">
+                <h1 className="text-white text-center display-2 bg-blurple rounded py-2">
                   Outschool Rebus Puzzles
                 </h1>
-                <h4 className="lead d-flex justify-content-center">
+                <h4 className="lead text-center">
                   Challenge your friends and coworkers to some good-natured
                   competition.
                 </h4>
@@ -30,8 +30,8 @@ export default function StartMenu() {
               </Col>
             </Row>
             <Row className="align-middle pt-3 d-flex justify-content-center">
-              <Col className="d-flex justify-content-center" xs={4}>
-                <h3>Start a game:</h3>
+              <Col xs={4}>
+                <h3 className="text-center">Start a game:</h3>
               </Col>
               <Col
                 className="d-flex justify-content-center align-bottom"
@@ -41,8 +41,8 @@ export default function StartMenu() {
                 <h3>Add/delete rebus puzzles:</h3>
               </Col>
             </Row>
-            <Row className="align-middle pt-3 mb-2 d-flex justify-content-center">
-              <Col className="d-flex justify-content-center" xs={4}>
+            <Row className="align-middle pt-2 mb-1 d-flex justify-content-center">
+              <Col className="d-flex justify-content-center" xs={5} md={4}>
                 <Button tag={Link} color="blurple" to="/play">
                   Play!
                 </Button>
@@ -57,7 +57,7 @@ export default function StartMenu() {
                   <SingleEmoji shortName="sparkles" />
                 </h4>
               </Col>
-              <Col className="d-flex justify-content-center" xs={4}>
+              <Col className="d-flex justify-content-center" xs={5} md={4}>
                 <Button tag={Link} color="blurple" to="/manage">
                   Manage!
                 </Button>
@@ -66,13 +66,16 @@ export default function StartMenu() {
           </Jumbotron>
         </Col>
       </Row>
-      <Row className="py-2 px-1">
-        <Col className="px-4 mx-2 " xs={10} />
-        <Col className="px-4 mx-2 " xs={1}>
-          <Button className="btn-blurple px-4 py-2" href="/auth/logout">
+      <Row className="py-2 px-1 d-flex justify-content-end ">
+        <Col xs={4} md={10}>
+          {" "}
+        </Col>
+        <Col className="px-1 mx-2 d-flex justify-content-end" xs={4} md={2}>
+          <Button className="btn-blurple px-4 py-2 mb-4" href="/auth/logout">
             Logout
           </Button>
         </Col>
+        <Col xs={1} />
       </Row>
     </>
   );
